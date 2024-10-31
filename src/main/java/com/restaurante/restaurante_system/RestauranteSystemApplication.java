3443b6bac5da8ec4a3aa1adeded4ab012186f359
Jorge Maia <91641055391@tjce.jus.br>
2024-10-30 21:55:05 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/com/restaurante/restaurante_system/RestauranteSystemApplication.java
@@ -0,0 +1,13 @@
+package com.restaurante.restaurante_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteSystemApplication.class, args);
+	}
+
+}
